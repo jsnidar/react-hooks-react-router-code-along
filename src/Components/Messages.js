3c4627c11ceb5151f2cedb3a9731c26f92643b21
Jeremiah <jsnidar@gmail.com>
2021-09-21 21:30:54 -0500

    commit tree 8bb22afe4c5900aadfb77ebad4b38d39e9e2ca48

---
+++ src/Components/Messages.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Messages () {
+
+    return (
+      <div>
+        <h3>Messages</h3>
+        <ul>
+          <li>Message 1</li>
+          <li>Message 2</li>
+        </ul>
+      </div>
+    )
+  }
+
+export default Messages;
